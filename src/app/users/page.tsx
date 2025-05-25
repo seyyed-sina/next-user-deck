ded1e43d4c69a21756ae61eea59f7054630fc936
sina <cna_fp@yahoo.com>
2025-05-25 20:40:53 +0330

    feat: wrap FavoriteList and UserList components in Suspense for improved loading handling

--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -1,12 +1,17 @@
 import { UserList } from "@/components/features/UserList/UserList";
 import type { Metadata } from "next";
+import { Suspense } from "react";
 
 export const metadata: Metadata = {
   title: "User List",
 };
 
 const UserListPage = () => {
-  return <UserList />
+  return (
+    <Suspense>
+      <UserList />
+    </Suspense>
+  );
 };
 
 export default UserListPage;
